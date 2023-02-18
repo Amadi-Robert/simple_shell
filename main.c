dd27ef4d5ef7325af6567385a4d0b60dde29dfd9
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-02-18 14:45:05 +0100

    simple shell unix files

--- main.c
+++ main.c
@@ -10,17 +10,17 @@ int main(int ac, char **argv)
 	int num_tokens = 0;
 	char *token;
 	int i;
-	
+
 	/* declaring void variables */
 	(void)ac;
-	
+
 	/* Create a loop for the shell's prompt */
-	while(1)
+	while (1)
 	{
 		printf("%s", prompt);
 		nchars_read = getline(&lineptr, &n, stdin);
-		
-		/* check if the getline function failed or reached EOF or user use CTRL + D */
+
+/* check if the getline function failed or reached EOF or user use CTRL + D */
 		if (nchars_read == -1)
 		{
 			printf("Exiting....\n");
@@ -35,45 +35,43 @@ int main(int ac, char **argv)
 		}
 	/* copy lineptr to lineptr_copy */
 		strcpy(lineptr_copy, lineptr);
-		
-	/**
-	 * plitting the string (lineptr) into an array of words
-	 * calculate the total number of tokens 
-	 */
+
+/**
+ * plitting the string (lineptr) into an array of words
+ * calculate the total number of tokens 
+ */
 		
 	token = strtok(lineptr, delim);
-		
+
 		while (token != NULL)
 		{
 			num_tokens++;
 			token = strtok(NULL, delim);
 		}
 		num_tokens++;
-		
+
 	/* Allocate space to hold the array of strings */
 	argv = malloc(sizeof(char *) * num_tokens);
-		
+
 	/* Store each token in the argv array */
 	token = strtok(lineptr_copy, delim);
-		
+
 		for (i = 0; token != NULL; i++)
 		{
 			argv[i] = malloc(sizeof(char) * strlen(token));
 			strcpy(argv[i], token);
-			
+
 			token = strtok(NULL, delim);
 		}
 		argv[i] = NULL;
-		
+
 	/* execute the command */
 		execmd(argv);
 	}
-			
-	/* free up allocated memory */
+	
+/* free up allocated memory */
 	free(lineptr_copy);
 	free(lineptr);
 
 	return(0);
-
 }
-
